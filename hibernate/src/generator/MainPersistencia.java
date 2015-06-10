5cc7e9efb6e278c22d7594559923b294e4ecdf4b
buskador007 <andrescostaribeiro16@gmail.com>
2015-06-10 16:35:16 +0200

    asd

--- hibernate/src/generator/MainPersistencia.java
+++ hibernate/src/generator/MainPersistencia.java
@@ -14,7 +14,14 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.type.descriptor.java.UUIDTypeDescriptor.ToStringTransformer;
-
+/*
+ * @author: jorge andre da costa ribeiro
+ * @date: 10/6/2015
+ * 
+ * crear un metodo que interactue con la BBDD, que aumente el salario de los empleados del departamento de ventas un 20%
+ * 
+ * version: 1.0
+ * */
 
 
 public class MainPersistencia {
@@ -27,41 +34,70 @@ public class MainPersistencia {
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
 		SessionFactory factory = configuration.buildSessionFactory(builder.build());
 		
+		/*primero me creo una nueva configuracion para llamar al fichero de config de hibernate
+		 * despues me creo un builder para que reciba el registro para hacer un factory
+		 * y al final me creo un factory para la sesion 
+		 * */
 		
-		Session sesion1 = null;
-		Transaction trasacion = null;
+		Session sesion1 = null;           //declaro una variable de sesion
+		Transaction trasacion = null;    //y me declaro una variable de transaccion 
 		
 		try{
-			sesion1 = factory.openSession();
-			trasacion = sesion1.beginTransaction();
+			sesion1 = factory.openSession();           //abro la sesion con el factory
+			trasacion = sesion1.beginTransaction();   //y abro con la sesion antes creada una transaccion
 			@SuppressWarnings("unchecked")
 			List<Employees> list = sesion1.createSQLQuery("select * from employees where department_id = 80").addEntity(Employees.class).list();
+			
+				//delclaro una lista de empleados para meter ahi toda la imformacion de la query planteada a la BBDD.
+
 			Iterator<Employees> it = list.iterator();
+			
+				//me declaro un interador para interactuar con la lista y sacar info
+			
 			Employees empleado;
 			BigDecimal big = null;
+			
+				//declaro las variable de empleado para saber en que empleado voy
+				//y una variable de BIGDECIMAL para el sueldo final y pasarlo a la BBDD
+			
 			while(it.hasNext()){
 				
+				//con el bucle quiero hacer que mientra que el interador tenga siguiente haga lo siguiente
+				
 				empleado = it.next();
+				
+				//empleado toma valor del interador
+				
 				nuevosalario = (int)(empleado.getSalary().doubleValue() * porcentaje );
+				
+				//calculo el salario como int cogiendo el salario de empleado y lo cambio a  double
+				//y despues lo multiplico por el porcentaje delcarado al principio
+				
 				System.out.println("el empleado " + empleado + " tendra un salario final " + nuevosalario);
+				
+				//imprimo en pantalla la frase para saber que ha llegado a "buen puerto" el calculo
 				
 				big = new BigDecimal(nuevosalario);
 				
-				empleado.setSalary(big);
-				sesion1.update(empleado);
-				System.out.println("se ha seteado correctamente");
+				//cambio la variable big que toma valo nuevo salario
+				
+				empleado.setSalary(big);			//seteo que el salario que toma el valor de big
+				
+				sesion1.update(empleado);			//actualizo la sesion para que se guarde
+				System.out.println("se ha seteado correctamente");	//printeo en pantalla para saber "que se completo"
 				
 			}
-			trasacion.commit();
+			trasacion.commit();		//hago un comit para guardalo "ya" en la BBDD
 		}
-		catch(Exception e){
+		catch(Exception e){			//si falla algo de lo anterior viene "Aqui"
 			e.printStackTrace();
-			trasacion.rollback();
+			trasacion.rollback();	//se ejecuta un rollback para a deshacer
 			
 		}
 		finally{
-			sesion1.disconnect();
-			factory.close();
+				//si todo va bien llega a "aqui"
+			sesion1.disconnect();		//me desconecto de la sesion
+			factory.close();			//y cierro el factory
 		}
 		
 	}
